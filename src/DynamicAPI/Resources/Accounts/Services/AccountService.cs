2eeac9312b36f9ab8aa4e53eab90338aa46d56fb
Geoffrey Herpin <geoffrey.herpin@extendhealth.com>
2021-04-30 16:08:04 -0600

    Added some more additional data and a basic in memory paging to simulate database paging

--- src/DynamicAPI/Resources/Accounts/Services/AccountService.cs
+++ src/DynamicAPI/Resources/Accounts/Services/AccountService.cs
@@ -17,8 +17,18 @@ namespace DynamicAPI.Resources.Accounts.Services {
     {
         public async Task<AccountsQueryResponse> GetAccounts(int page, int perPage)
         {
+            var accounts = await Accounts();
+            var accountPages = new List<List<AccountDataModel>>();
+
+            if (perPage > 0) {
+                 accountPages = accounts.Select((x,i) => new { Index = i, Value = x})
+                                .GroupBy(x => x.Index / perPage)
+                                .Select(x => x.Select(v => v.Value).ToList())
+                                .ToList();
+            }
+           
             var accountQueryResponse = new AccountsQueryResponse();
-            accountQueryResponse.Accounts = await Accounts();
+            accountQueryResponse.Accounts = perPage == 0 ? accounts : accountPages[page];
             accountQueryResponse.TotalCount = accountQueryResponse.Accounts.Count();
             return accountQueryResponse;
         }
@@ -57,7 +67,70 @@ namespace DynamicAPI.Resources.Accounts.Services {
                     PropertyFour = "Property Four",
                     PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
                     PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
-                }
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("b0689a1c-6633-4f8c-bf4f-4d798e479371"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("93a5f780-3c16-45d6-be5a-347024b44698"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("e8dede7a-18de-40d1-a45d-51289cbb9ebc"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("9b6b2078-d4b6-40df-b49c-c4fa08329b53"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("2fd2a3ee-0b05-447c-88d1-a66376d2e8ba"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("9fa0246e-9ff8-46f0-ac6f-99fde2282b43"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
+                 new AccountDataModel {
+                    Id = Guid.Parse("1f044955-6889-4bc8-b826-28a6648dc4b4"),
+                    PropertyOne = "Property One",
+                    PropertyTwo = "Property Two",
+                    PropertyThree = "Property Three",
+                    PropertyFour = "Property Four",
+                    PropertyFive = new List<string> {"one", "two", "three", "four", "five"},
+                    PropertySix = new AccountOwnership { PropertyOne = "Property One", PropertyTwo = "Property Two", PropertyThree = "Property Three "}
+                },
             };
 
             return await Task.FromResult(accounts);
